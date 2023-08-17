07e24761141a77d96279d5b85bee12ec1d3ae2e3
Geert Bevin <gbevin@uwyn.com>
2023-08-17 10:57:53 -0400

    Updated for bld 1.7.1

--- src/bld/java/rife/bld/extension/ArchiveBuild.java
+++ src/bld/java/rife/bld/extension/ArchiveBuild.java
@@ -19,7 +19,7 @@ public class ArchiveBuild extends Project {
     public ArchiveBuild() {
         pkg = "rife.bld.extension";
         name = "Archive";
-        version = version(0,4,0);
+        version = version(0,4,1);
         archiveBaseName = "bld-archive";
 
         javaRelease = 17;
@@ -28,7 +28,7 @@ public class ArchiveBuild extends Project {
 
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
         scope(compile)
-            .include(dependency("com.uwyn.rife2", "bld", version(1,7,0)))
+            .include(dependency("com.uwyn.rife2", "bld", version(1,7,1)))
             .include(dependency("org.apache.commons", "commons-compress", version(1,23,0)));
         scope(test)
             .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,9,3)))
