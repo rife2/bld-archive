c816a564ed0cf98c74e2ca69b035a50c530998bd
Geert Bevin <gbevin@uwyn.com>
2024-02-25 22:44:29 -0500

    Updated for bld 1.9.0. Updated version to 0.4.7.

--- src/bld/java/rife/bld/extension/ArchiveBuild.java
+++ src/bld/java/rife/bld/extension/ArchiveBuild.java
@@ -19,7 +19,7 @@ public class ArchiveBuild extends Project {
     public ArchiveBuild() {
         pkg = "rife.bld.extension";
         name = "Archive";
-        version = version(0,4,6);
+        version = version(0,4,7);
         archiveBaseName = "bld-archive";
 
         javaRelease = 17;
@@ -28,11 +28,11 @@ public class ArchiveBuild extends Project {
 
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
         scope(compile)
-            .include(dependency("com.uwyn.rife2", "bld", version(1,8,0)))
-            .include(dependency("org.apache.commons", "commons-compress", version(1,25,0)));
+            .include(dependency("com.uwyn.rife2", "bld", version(1,9,0)))
+            .include(dependency("org.apache.commons", "commons-compress", version(1,26,0)));
         scope(test)
-            .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,10,1)))
-            .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1,10,1)));
+            .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,10,2)))
+            .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1,10,2)));
 
         javadocOperation()
             .javadocOptions()
